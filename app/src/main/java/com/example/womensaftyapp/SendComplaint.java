9438cfddc3d13c07367e53684eb90f0aa3f855d0
nithinsmac <nithin@applab.qa>
2023-02-23 16:41:24 +0530

    Third review

---
+++ app/src/main/java/com/example/womensaftyapp/SendComplaint.java
@@ -0,0 +1,14 @@
+package com.example.womensaftyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_complaint);
+    }
+}
--- app/src/main/java/com/example/womensaftyapp/UserHome.java
+++ app/src/main/java/com/example/womensaftyapp/UserHome.java
@@ -1,25 +1,102 @@
 package com.example.womensaftyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.lang.Double.parseDouble;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.womensaftyapp.databinding.ActivityAddPoliceBinding;
 import com.example.womensaftyapp.databinding.ActivityUserHomeBinding;
+import com.example.womensaftyapp.settings.LocationMonitoringService;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class UserHome extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class UserHome extends FragmentActivity implements OnMapReadyCallback {
 ActivityUserHomeBinding binding;
+    private GoogleMap mMap;
+    String latitude, longitude;
+    Boolean b = false;
+    FirebaseFirestore db;
+    ProgressDialog progressDoalog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityUserHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        db = FirebaseFirestore.getInstance();
+        progressDoalog = new ProgressDialog(UserHome.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.setTitle("Please wait");
+        progressDoalog.setCancelable(false);
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDoalog.show();
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        latitude = intent.getStringExtra(LocationMonitoringService.EXTRA_LATITUDE);
+                        longitude = intent.getStringExtra(LocationMonitoringService.EXTRA_LONGITUDE);
+                        //Toast.makeText(ChooseActivity.this, latitude + longitude + "", Toast.LENGTH_SHORT).show();
+                        if (latitude != null && longitude != null) {
+//                            LatLng latLng = new LatLng(parseDouble(latitude), parseDouble(longitude));
+//                            CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(12).build();
+//                            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//                            mMap.clear();
+//                            mMap.addMarker(new MarkerOptions()
+//                                    .position(latLng)
+//                                    .title("you are Here")
+//                                    .icon(BitmapDescriptorFactory
+//                                            .defaultMarker(BitmapDescriptorFactory.HUE_RED))).showInfoWindow();
+                            if (b == false) {
+                                mMap.clear();
+                                showData();
+                                b = true;
+                            }
+
+                        }
+                    }
+                }, new IntentFilter(LocationMonitoringService.ACTION_LOCATION_BROADCAST));
+
+
+
+
         binding.sos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +111,21 @@ ActivityUserHomeBinding binding;
             }
         });
 
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,5 +155,68 @@ ActivityUserHomeBinding binding;
     public void onBackPressed() {
         finish();
         moveTaskToBack(true);
+    }
+    private void showData() {
+
+        //Log.d("@", "showData: Called")
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("User").whereEqualTo("utype", "Police")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        int i;
+                        for (i = 0; i < queryDocumentSnapshots.getDocuments().size(); i++) {
+
+                            try {
+                                    if (latitude != null && longitude != null) {
+                                        float[] results = new float[1];
+                                        Location.distanceBetween(parseDouble(latitude), parseDouble(longitude),
+                                                parseDouble(queryDocumentSnapshots.getDocuments().get(i).getString("hlatitude")), parseDouble(queryDocumentSnapshots.getDocuments().get(i).getString("hlongitude")),
+                                                results);
+                                        float km = results[0] / 1000;
+                                        LatLng latLng = new LatLng(Double.parseDouble(queryDocumentSnapshots.getDocuments().get(i).getString("hlatitude")), Double.parseDouble(queryDocumentSnapshots.getDocuments().get(i).getString("hlongitude")));
+                                        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(12).build();
+                                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                                        //  mMap.clear();
+
+                                        mMap.addMarker(new MarkerOptions()
+                                                .position(latLng)
+                                                .title("Hospital Name-\t" + queryDocumentSnapshots.getDocuments().get(i).getString("name")
+                                                        + ":"+queryDocumentSnapshots.getDocuments().get(i).getString("address")
+                                                        + "\t:\tDistance from you-" + km+"\t:\t\tHospital Phone-"+queryDocumentSnapshots.getDocuments().get(i).getString("phone")+
+                                                        "Hospital ID:"+queryDocumentSnapshots.getDocuments().get(i).getId())
+                                                .icon(BitmapDescriptorFactory
+                                                        .defaultMarker(BitmapDescriptorFactory.HUE_AZURE))).showInfoWindow();
+
+                                    }
+                                    mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                                        public void onInfoWindowClick(Marker marker) {
+                                            SharedPreferences sd=getSharedPreferences("police",Context.MODE_PRIVATE);
+                                            SharedPreferences.Editor ed=sd.edit();
+                                            ed.putString("policestation",marker.getTitle());
+                                            ed.commit();
+                                            startActivity(new Intent(getApplicationContext(), SendComplaint.class));
+                                            finish();
+                                        }
+                                    });
+
+                            } catch (Exception e) {
+                            }
+
+                        }
+                        progressDoalog.dismiss();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
